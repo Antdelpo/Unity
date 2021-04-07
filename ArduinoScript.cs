629bfec8a546a24d4e700c25cadc2fe4cef81039
Anthony <70208221+Antdelpo@users.noreply.github.com>
2021-04-07 13:58:59 -0400

    Update ArduinoScript.cs

--- ArduinoScript.cs
+++ ArduinoScript.cs
@@ -5,15 +5,13 @@ using System.IO.Ports;
 
 public class ArduinoScript : MonoBehaviour
 {
-
 	SerialPort sp= new SerialPort("/dev/cu.usbmodem1101",9600);
 	public float movementModifier;
 
-    // Start is called before the first frame update
     void Start()
     {
 	sp.Open();
-        	sp.ReadTimeout = 1;
+        sp.ReadTimeout = 1;
     }
 
     // Update is called once per frame
